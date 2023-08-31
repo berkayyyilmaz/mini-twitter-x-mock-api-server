b43ae3f2d8b4d1076d0e6d1cecf93cf2030b7191
Gökhan <gn.ozdemir@gmail.com>
2023-09-01 01:18:35 +0300

    Complete Documentation

--- README.md
+++ README.md
@@ -1,80 +1,69 @@
-# Sample API Responses & Request Data Objects:
+# Mini Twitter X - API Server
 
-## Profile
+## How to run
 
-**Register**
+### Install dependencies
 
-- **Endpoint**: `${URL}/profile/register`
-- **Method**: POST
+```bash
+npm install
+```
 
-**Request**:
+### Run the server
 
-```json
-{
-  "username": "exampleUser",
-  "password": "examplePass123",
-  "email": "user@example.com"
+```bash
+npm run api
+```
+
+will run the server on `http://localhost:3001`
+
+When you run the server, it will console log 3 predefined users. You can use one of them to login. You can also register a new user.
+
+You can use the following user to login:
+
+```bash
+Predefined user: {
+  id: '4iqOO',
+  username: 'Tatum_Schneider',
+  password: 'oaZSxaOPcQfzhF6',
+  email: 'Marielle76@gmail.com',
+  hashedPassword: '$2b$10$DVy/a7kxN...6BbZZh1O',
+  token: 'eyJ...vd49g'
 }
 ```
 
-**Response**:
+## Endpoints & Requests:
 
-```json
-{
-  "status": "success",
-  "message": "User registered successfully.",
-  "data": {
-    "id": 1,
-    "username": "exampleUser",
-    "email": "user@example.com"
-  }
-}
-```
+**Profile Authentication:**
 
-**Giriş Yap (Login)**
+| Method | URL                       | Desc     |
+| ------ | ------------------------- | -------- |
+| POST   | `${URL}/profile/register` | Register |
+| POST   | `${URL}/profile/logout`   | Logout   |
+| POST   | `${URL}/profile/login`    | Login    |
 
-- **Endpoint**: `${URL}/profile/login`
-- **Method**: POST
+**Tweet İşlemleri:**
 
-**Request**:
+There urls are protected by authentication. You need to send `Authorization` header with `Bearer <token>` value.
 
-```json
-{
-  "username": "exampleUser",
-  "password": "examplePass123"
-}
-```
+| Method | URL                       | Description    |
+| ------ | ------------------------- | -------------- |
+| GET    | `${URL}/tweet/`           | Get All Tweets |
+| GET    | `${URL}/tweet/id`         | Get a Tweet    |
+| POST   | `${URL}/tweet`            | New Tweet      |
+| PUT    | `${URL}/tweet/id`         | Edit Tweet     |
+| DELETE | `${URL}/tweet/id`         | Delete Tweet   |
+| POST   | `${URL}/tweet/like/id`    | Like           |
+| DELETE | `${URL}/tweet/like/id`    | Unlike         |
+| POST   | `${URL}/tweet/retweet/id` | Retweet        |
+| POST   | `${URL}/tweet/reply/id`   | Comment        |
+| DELETE | `${URL}/tweet/reply/id`   | Delete Comment |
 
-**Response**:
+## Dynamic Documentation
 
-```json
-{
-  "status": "success",
-  "message": "Login successful.",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
-}
-```
+## [Profile](https://www.postman.com/warped-escape-979153/workspace/mini-twitter-x-mock-api/documentation/391613-69e7dcd3-f015-46fc-bfe6-ca07872f5c00)
 
-**Çıkış Yap (Logout)**
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/391613-69e7dcd3-f015-46fc-bfe6-ca07872f5c00?action=collection%2Ffork&source=rip_markdown&collection-url=entityId%3D391613-69e7dcd3-f015-46fc-bfe6-ca07872f5c00%26entityType%3Dcollection%26workspaceId%3D59104268-2598-42ee-bcde-aadc73db1540#?env%5BMini%20Twitter%20X%20WiT%20Mock%20API%5D=W3siZW5hYmxlZCI6dHJ1ZSwia2V5IjoidXJsIiwidmFsdWUiOiJodHRwczovLzcxNTI1Yjk5LTQ1NWEtNDViZS1iZmViLTFiOWU4Mjg0YzA4YS5tb2NrLnBzdG1uLmlvIiwidHlwZSI6InRleHQifV0=)
 
-- **Endpoint**: `${URL}/profile/logout`
-- **Method**: POST
+## [Tweet Management](https://www.postman.com/warped-escape-979153/workspace/mini-twitter-x-mock-api/documentation/391613-745c5287-8164-4db7-9c19-ce90608075c2)
 
-**Request**:
-
-```json
-{
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
-}
-```
-
-**Response**:
-
-```json
-{
-  "status": "success",
-  "message": "Logged out successfully."
-}
-```
-
-## Tweet
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/391613-745c5287-8164-4db7-9c19-ce90608075c2?action=collection%2Ffork&source=rip_markdown&collection-url=entityId%3D391613-745c5287-8164-4db7-9c19-ce90608075c2%26entityType%3Dcollection%26workspaceId%3D59104268-2598-42ee-bcde-aadc73db1540#?env%5BMini%20Twitter%20X%20WiT%20Mock%20API%5D=W3siZW5hYmxlZCI6dHJ1ZSwia2V5IjoidXJsIiwidmFsdWUiOiJodHRwczovLzcxNTI1Yjk5LTQ1NWEtNDViZS1iZmViLTFiOWU4Mjg0YzA4YS5tb2NrLnBzdG1uLmlvIiwidHlwZSI6InRleHQifV0=)
--- server.mjs
+++ server.mjs
@@ -1,6 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+
+// Import the tweets data in required format
+
+// import tweets.json from data/tweets.json and init server with that seed data
+import tweetsData from "./data/tweets.json" assert { type: "json" };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,9 +16,64 @@ app.use(express.json());
 
 // In-memory data storage
 const users = [];
-const tokens = [
-  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjkzNTA0NTE4LCJleHAiOjE2OTM1MDgxMTh9.IUlyLJAcrqT0lwquK5-mD_tPdmrT7ndibnr6CiRZ5cg",
-];
+
+const tokens = [];
+function initializePredefinedUsers() {
+  const predefinedUsers = [
+    {
+      id: nanoid(5),
+      username: "Ephraim21",
+      password: "l_dxA27E49g6pIy",
+      email: "Cristal_DAmore@yahoo.com",
+    },
+    {
+      id: nanoid(5),
+      username: "Lloyd27",
+      password: "IESEqL_B87wrgR5",
+      email: "Colin20@gmail.com",
+    },
+    {
+      id: nanoid(5),
+      username: "Tatum_Schneider",
+      password: "oaZSxaOPcQfzhF6",
+      email: "Marielle76@gmail.com",
+    },
+  ];
+
+  predefinedUsers.forEach(async (user) => {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const token = jwt.sign({ id: user.id }, "SECRET_KEY", {
+      expiresIn: "12h",
+      // Generate and store the token
+    });
+    tokens.push(token);
+
+    // Store the user with hashed password
+    const updatedUser = {
+      ...user,
+      hashedPassword,
+      token,
+    };
+    users.push(updatedUser);
+    console.log("Predefined user:", updatedUser);
+  });
+  console.log("Predefined users initialized successfully.");
+}
+
+// Initialize predefined users
+initializePredefinedUsers();
+console.log("Predefined users:", users);
+
+// ----------------------
+// Sample Route
+// ----------------------
+
+// Sample route to test the server
+app.get("/", (req, res) => {
+  res.send("Hello, Workintech Student! Mini Twitter X API Server is running");
+});
 
 // Register endpoint
 app.post("/profile/register", async (req, res) => {
@@ -32,10 +93,11 @@ app.post("/profile/register", async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = {
-    id: users.length + 1,
+    id: nanoid(5),
     username,
     email,
-    password: hashedPassword,
+    password,
+    hashedPassword,
   };
 
   users.push(newUser);
@@ -44,7 +106,7 @@ app.post("/profile/register", async (req, res) => {
     status: "success",
     message: "User registered successfully.",
     data: {
-      id: newUser.id,
+      id: newUser.username,
       username: newUser.username,
       email: newUser.email,
     },
@@ -55,14 +117,18 @@ app.post("/profile/register", async (req, res) => {
 app.post("/profile/login", async (req, res) => {
   const { username, password } = req.body;
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = users.find((u) => u.username === username);
-  if (!user || !(await bcrypt.compare(password, user.password))) {
+  if (!user || !(await bcrypt.compare(password, user.hashedPassword))) {
     return res
       .status(400)
       .json({ status: "error", message: "Invalid username or password." });
   }
 
-  const token = jwt.sign({ id: user.id }, "SECRET_KEY", { expiresIn: "12h" });
+  const token = jwt.sign({ id: user.id }, "SECRET_KEY", {
+    expiresIn: "12h",
+  });
   tokens.push(token);
 
   res.json({
@@ -95,11 +161,11 @@ app.post("/profile/logout", (req, res) => {
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  console.log(
+  /* console.log(
     "request Token",
     tokens,
     `${token?.slice(0, 5)}...${token?.slice(token?.length - 5)}, ${token}`
-  );
+  ); */
   if (!token)
     return res
       .status(401)
@@ -116,14 +182,16 @@ const authenticateToken = (req, res, next) => {
 };
 
 // In-memory data storage
-const tweets = [];
+
+const tweets = [...tweetsData];
 
 // Get all tweets
 app.post("/tweet", authenticateToken, (req, res) => {
   const { content } = req.body;
 
   const newTweet = {
-    id: tweets.length + 1,
+    id: nanoid(5),
+    userId: req.user.username,
     content,
     likes: 0,
     retweets: 0,
@@ -139,10 +207,191 @@ app.post("/tweet", authenticateToken, (req, res) => {
   });
 });
 
+// get all tweets
+app.get("/tweet", authenticateToken, (req, res) => {
+  res.json({
+    status: "success",
+    message: "Tweets retrieved successfully.",
+    data: tweets,
+  });
+});
+
+// Get a single tweet
+app.get("/tweet/:id", authenticateToken, (req, res) => {
+  const tweetId = req.params.id;
+  console.log(tweetId);
+  const tweet = tweets.find((t) => t.id === tweetId.toString());
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+  }
+
+  res.json({
+    status: "success",
+    message: "Tweet retrieved successfully.",
+    data: tweet,
+  });
+});
+
+// Edit a tweet
+app.put("/tweet/:id", authenticateToken, (req, res) => {
+  const tweetId = req.params.id;
+  const { content } = req.body;
+
+  const tweet = tweets.find((t) => t.id === tweetId);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+  }
+
+  tweet.content = content;
+
+  res.json({
+    status: "success",
+    message: "Tweet updated successfully.",
+    data: tweet,
+  });
+});
+
+// Delete a tweet
+app.delete("/tweet/:id", authenticateToken, (req, res) => {
+  const tweetId = req.params.id;
+
+  const tweet = tweets.find((t) => t.id === tweetId);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+
+    // Check if the user is the owner of the tweet
+    if (tweet.username !== req.user.username) {
+      return res.status(403).json({
+        status: "error",
+        message: "You are not authorized to delete this tweet.",
+      });
+    }
+  }
+
+  const index = tweets.indexOf(tweet);
+  tweets.splice(index, 1);
+
+  res.json({
+    status: "success",
+    message: "Tweet deleted successfully.",
+  });
+});
+
+// Like a tweet
+app.post("/tweet/like/:id", authenticateToken, (req, res) => {
+  const tweetId = req.params.id;
+
+  const tweet = tweets.find((t) => t.id === tweetId);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+  }
+
+  tweet.likes = tweet.likes + 1;
+
+  res.json({
+    status: "success",
+    message: "Tweet liked successfully.",
+  });
+});
+
+// Unlike a tweet
+app.delete("/tweet/like/:id", authenticateToken, (req, res) => {
+  const tweetId = req.params.id;
+
+  const tweet = tweets.find((t) => t.id === tweetId);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+  }
+
+  tweet.likes = tweet.likes > 0 ? tweet.likes - 1 : 0;
+
+  res.json({
+    status: "success",
+    message: "Tweet unliked successfully.",
+  });
+});
+
+// Reply to a tweet
+app.post("/tweet/reply/:id", authenticateToken, (req, res) => {
+  const tweetId = req.params.id;
+  const { content, username } = req.body;
+
+  const tweet = tweets.find((t) => t.id === tweetId);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+  }
+
+  // Create a new tweet for the reply
+  const reply = {
+    id: `${tweetId}__${nanoid(5)}`,
+    content,
+    likes: 0,
+    retweets: 0,
+    username,
+  };
+
+  tweet.replies.push(reply);
+
+  tweets.push(reply);
+
+  res.json({
+    status: "success",
+    message: "Tweet replied successfully.",
+  });
+});
+
+// Delete a reply
+app.delete("/tweet/reply/:replyid", authenticateToken, (req, res) => {
+  const replyId = req.params.replyid;
+  const tweetId = req.params.replyid.split("__")[0];
+
+  const tweet = tweets.find((t) => t.id === tweetId);
+
+  if (!tweet) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Tweet not found." });
+  }
+
+  const reply = tweet.replies.find((r) => r.id === replyId);
+
+  if (!reply) {
+    return res
+      .status(404)
+      .json({ status: "error", message: "Reply not found." });
+  }
+
+  tweet.replies = tweet.replies.filter((r) => r.id !== replyId);
+
+  res.json({
+    status: "success",
+    message: "Reply deleted successfully.",
+  });
+});
+
 // Retweet a tweet
 app.post("/tweet/retweet/:id", authenticateToken, (req, res) => {
-  const tweetId = parseInt(req.params.id);
-  const { userId } = req.body;
+  const tweetId = req.params.id;
+  const { username } = req.body;
+  console.log(tweetId, username);
 
   const originalTweet = tweets.find((t) => t.id === tweetId);
 
@@ -154,7 +403,7 @@ app.post("/tweet/retweet/:id", authenticateToken, (req, res) => {
 
   // Check if the user has already retweeted
   const userRetweet = tweets.find(
-    (t) => t.retweetedFrom === tweetId && t.userId === userId
+    (t) => t.retweetedFrom === tweetId && t.username === username
   );
   if (userRetweet) {
     return res.status(400).json({
@@ -168,12 +417,12 @@ app.post("/tweet/retweet/:id", authenticateToken, (req, res) => {
 
   // Create a new tweet for the retweet
   const retweet = {
-    id: tweets.length + 1,
+    id: nanoid(5),
     content: originalTweet.content, // Copying content from original tweet
     likes: 0,
     retweets: 0,
     replies: [],
-    userId, // The user who retweeted
+    username, // The user who retweeted
     retweetedFrom: originalTweet.id, // Reference to the original tweet
   };
 
@@ -183,15 +432,6 @@ app.post("/tweet/retweet/:id", authenticateToken, (req, res) => {
     status: "success",
     message: "Tweet retweeted successfully.",
   });
-});
-
-// ----------------------
-// Sample Route
-// ----------------------
-
-// Sample route to test the server
-app.get("/", (req, res) => {
-  res.send("Hello, Workintech Student! Mini Twitter X API Server is running");
 });
 
 // Start the server
